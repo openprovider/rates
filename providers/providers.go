e3ea4480154e5bfe6dc01d8386c392018187c7be
Igor Dolzhikov <bluesriverz@gmail.com>
2016-06-01 23:48:52 +0600

    sharing types of the currencies between providers

---
+++ providers/providers.go
@@ -0,0 +1,46 @@
+// Copyright 2016 Openprovider Authors. All rights reserved.
+// Use of this source code is governed by a license
+// that can be found in the LICENSE file.
+
+package providers
+
+// List of all supported currencies
+const (
+	AUD = "AUD" // Australian Dollar (A$)
+	BGN = "BGN" // Bulgarian Lev (BGN)
+	BRL = "BRL" // Brazilian Real (R$)
+	CAD = "CAD" // Canadian Dollar (CA$)
+	CHF = "CHF" // Swiss Franc (CHF)
+	CNY = "CNY" // Chinese Yuan (CN¥)
+	CZK = "CZK" // Czech Republic Koruna (CZK)
+	DKK = "DKK" // Danish Krone (DKK)
+	EUR = "EUR" // Euro (€)
+	GBP = "GBP" // British Pound Sterling (£)
+	HKD = "HKD" // Hong Kong Dollar (HK$)
+	HRK = "HRK" // Croatian Kuna (HRK)
+	HUF = "HUF" // Hungarian Forint (HUF)
+	IDR = "IDR" // Indonesian Rupiah (IDR)
+	ILS = "ILS" // Israeli New Sheqel (₪)
+	INR = "INR" // Indian Rupee (Rs.)
+	JPY = "JPY" // Japanese Yen (¥)
+	KRW = "KRW" // South Korean Won (₩)
+	MXN = "MXN" // Mexican Peso (MX$)
+	MYR = "MYR" // Malaysian Ringgit (MYR)
+	NOK = "NOK" // Norwegian Krone (NOK)
+	NZD = "NZD" // New Zealand Dollar (NZ$)
+	PHP = "PHP" // Philippine Peso (Php)
+	PLN = "PLN" // Polish Zloty (PLN)
+	RON = "RON" // Romanian Leu (RON)
+	RUB = "RUB" // Russian Ruble (RUB)
+	SEK = "SEK" // Swedish Krona (SEK)
+	SGD = "SGD" // Singapore Dollar (SGD)
+	THB = "THB" // Thai Baht (฿)
+	TRY = "TRY" // Turkish Lira (TRY)
+	USD = "USD" // US Dollar ($)
+	ZAR = "ZAR" // South African Rand (ZAR)
+
+	// Standard Date/Time template for exchange rates
+	stdDateTime = "2006-01-02 15:04:05"
+	// Standard Date template for exchange rates
+	stdDate = "2006-01-02"
+)
