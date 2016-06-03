e291b21fb16a28a9d3c7260c450763b4c9ce5c3b
Igor Dolzhikov <bluesriverz@gmail.com>
2016-06-03 11:53:46 +0600

    minor fixes for tabs

--- rates.go
+++ rates.go
@@ -35,39 +35,39 @@ Example 1: Get all exchange rates for the ECB Provider
 
 Example 2: Get exchange rates for EUR, USD, CHF, HKD
 
-	package main
+    package main
 
-	import (
-		"fmt"
+    import (
+        "fmt"
 
-		"github.com/openprovider/rates"
-		"github.com/openprovider/rates/providers"
-	)
+        "github.com/openprovider/rates"
+        "github.com/openprovider/rates/providers"
+    )
 
-	func main() {
-		registry := rates.Registry{
-			// any collection of providers which implement rates.Provider interface
-			providers.NewECBProvider(
-				&rates.Options{
-					Currencies: []string{
-						providers.EUR,
-						providers.USD,
-						providers.CHF,
-						providers.HKD,
-					},
-				},
-			),
-		}
-		service := rates.New(registry)
-		rates, errors := service.FetchLast()
-		if len(errors) != 0 {
-			fmt.Println(errors)
-		}
-		fmt.Println("European Central Bank exchange rates for today")
-		for index, rate := range rates {
-			fmt.Printf("%d. %s - %v\r\n", index+1, rate.Currency, rate.Value)
-		}
-	}
+    func main() {
+        registry := rates.Registry{
+            // any collection of providers which implement rates.Provider interface
+            providers.NewECBProvider(
+                &rates.Options{
+                    Currencies: []string{
+                        providers.EUR,
+                        providers.USD,
+                        providers.CHF,
+                        providers.HKD,
+                    },
+                },
+            ),
+        }
+        service := rates.New(registry)
+        rates, errors := service.FetchLast()
+        if len(errors) != 0 {
+            fmt.Println(errors)
+        }
+        fmt.Println("European Central Bank exchange rates for today")
+        for index, rate := range rates {
+            fmt.Printf("%d. %s - %v\r\n", index+1, rate.Currency, rate.Value)
+        }
+    }
 
 Exchange Rates Provider
 */
